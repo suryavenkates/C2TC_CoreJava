a7b6d1ed263348948fb5a844f2dfbba283fb34e3
Sarath <Sarath@LAPTOP-717GS3BG>
2023-02-16 15:26:13 +0530

    First Commit

---
+++ Day1Sample/src/com/cg/Day1Sample/hello/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.cg.Day1Sample.hello;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		// TODO Auto-generated method stub
+
+	}
+
+}
